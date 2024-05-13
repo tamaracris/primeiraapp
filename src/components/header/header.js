be9599a146b715ae54888f05b4a09ed6494264ed
Tamara Cristina <tamaracristina777@gmail.com>
2024-05-13 13:54:39 -0300

    alteração

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import './styles.css'
-import Logo from '../assets/logo.png';
+import Logo from '../../assets/logo.png';
 
 function Header({ }){
     return (
--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -1,7 +1,8 @@
 import React, { lazy, Suspense, useState } from 'react'
 import Header from '../../header/header';
 import './styles.css';
-import Video from '../../assets/videobt.mp4'
+import Video from '../../../assets/videobt.mp4'
+import Cards from '../../cards';
 
 
 function Home(){
@@ -10,16 +11,29 @@ function Home(){
         <>
         <Header/>
         <div id='banner'></div>
-        <div id='trailer-container' >
-            <div className='content' >
-                <video controls className='trailer' >
+        <div id='trailer-container'>
+            <div className='content'>
+                <video controls className='trailer'>
                     <source src={Video} />
-
+                    Seu navegador não possui suporte para vídeos
                 </video>
 
-            </div>
+                <div id='sinopse'>
+                    <p className='description'>
+                    Segue a história de Batman em seu segundo ano de luta contra o crime em busca de vingança, quando descobre a corrupção em Gotham City enquanto persegue o Charada/Enigma (Dano), um assassino em série que tem como alvo a elite de Gotham.
 
+                    </p>
+                    <button className='button'>Comprar ingresso</button>
+                    </div> 
+            </div>
         </div>
+        <Cards />
+
+        <footer>
+
+            <span>Todos os direitos reservados ©</span>
+            <span>Desenvolvido por Tamara Cristina</span>
+            </footer>
 
         </>
             )
