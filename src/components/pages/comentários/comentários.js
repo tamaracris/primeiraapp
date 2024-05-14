7774a12dd6002e2ef9a0a876533f8ad7f7b3b8bd
Tamara Cristina <tamaracristina777@gmail.com>
2024-05-14 00:22:15 -0300

    alteração

--- src/components/pages/comentários/comentários.js
+++ src/components/pages/comentários/comentários.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '../../header/header';
 
 function Comentários(){
     return (
         <>
+        <Header/>
                     <div id='sinopse'>
                     <p className='description'>
                     Segue a história de Batman em seu segundo ano de luta contra o crime em busca de vingança, quando descobre a corrupção em Gotham City enquanto persegue o Charada/Enigma (Dano), um assassino em série que tem como alvo a elite de Gotham.
